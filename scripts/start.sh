60c7265d324c10c02e93107d7049f4405e0b1777
Stan Svec <stan@taggstar.com>
2017-01-12 22:24:33 +0000

    wildcard in startup script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup java -jar /home/ec2-user/shodan/bin/shodan-0.2.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar /home/ec2-user/shodan/bin/shodan-*.jar > /dev/null 2> /dev/null < /dev/null &
